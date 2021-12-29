81b5d1f38ab03a53f058bd51148a4eabafb2c3b9
Zhao KK <kikakkz@hotmail.com>
2021-12-29 04:27:20 +0000

    Add create announcement

--- pkg/crud/announcement/announcement.go
+++ pkg/crud/announcement/announcement.go
@@ -2,12 +2,71 @@ package announcement
 
 import (
 	"context"
+	"time"
 
 	"github.com/NpoolPlatform/notification/message/npool"
+	"github.com/NpoolPlatform/notification/pkg/db"
+	"github.com/NpoolPlatform/notification/pkg/db/ent"
+
+	"github.com/google/uuid"
+
+	"golang.org/x/xerrors"
 )
 
+const (
+	dbTimeout = 5 * time.Second
+)
+
+func validateAnnouncement(info *npool.Announcement) error {
+	if _, err := uuid.Parse(info.GetAppID()); err != nil {
+		return xerrors.Errorf("invalid app id: %v", err)
+	}
+	if info.GetTitle() == "" {
+		return xerrors.Errorf("invalid title")
+	}
+	if info.GetContent() == "" {
+		return xerrors.Errorf("invalid content")
+	}
+	return nil
+}
+
+func dbRowToAnnouncement(row *ent.Announcement) *npool.Announcement {
+	return &npool.Announcement{
+		ID:       row.ID.String(),
+		AppID:    row.AppID.String(),
+		Title:    row.Title,
+		Content:  row.Content,
+		CreateAt: row.CreateAt,
+	}
+}
+
 func Create(ctx context.Context, in *npool.CreateAnnouncementRequest) (*npool.CreateAnnouncementResponse, error) {
-	return nil, nil
+	if err := validateAnnouncement(in.GetInfo()); err != nil {
+		return nil, xerrors.Errorf("invalid parameter: %v", err)
+	}
+
+	cli, err := db.Client()
+	if err != nil {
+		return nil, xerrors.Errorf("fail get db client: %v", err)
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, dbTimeout)
+	defer cancel()
+
+	info, err := cli.
+		Announcement.
+		Create().
+		SetAppID(uuid.MustParse(in.GetInfo().GetAppID())).
+		SetTitle(in.GetInfo().GetTitle()).
+		SetContent(in.GetInfo().GetTitle()).
+		Save(ctx)
+	if err != nil {
+		return nil, xerrors.Errorf("fail create announcement: %v", err)
+	}
+
+	return &npool.CreateAnnouncementResponse{
+		Info: dbRowToAnnouncement(info),
+	}, nil
 }
 
 func Update(ctx context.Context, in *npool.UpdateAnnouncementRequest) (*npool.UpdateAnnouncementResponse, error) {
