77a1eba1de816a77ede1316ffc7164c99e9ca4d3
fxnt <1330672764@qq.com>
2022-03-24 18:36:37 +0800

    add const

--- pkg/const/constant.go
+++ pkg/const/constant.go
@@ -4,3 +4,12 @@ const (
 	MessageTemplate = "{{ MESSAGE }}"
 	NameTemplate    = "{{ NAME }}"
 )
+
+const (
+	UsedForKycReviewApprovedNotification             = "KycReviewApprovedNotification"
+	UsedForKycReviewRejectedNotification             = "KycReviewRejectedNotification"
+	UsedForWithdrawReviewApprovedNotification        = "WithdrawReviewApprovedNotification"
+	UsedForWithdrawReviewRejectedNotification        = "WithdrawReviewRejectedNotification"
+	UsedForWithdrawAddressReviewApprovedNotification = "WithdrawAddressReviewApprovedNotification"
+	UsedForWithdrawAddressReviewRejectedNotification = "WithdrawAddressReviewRejectedNotification"
+)
