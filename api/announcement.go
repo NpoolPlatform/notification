46b8686a3f4f1ca438f1119afa3bb4eeab54d0e4
Zhao KK <kikakkz@hotmail.com>
2022-03-30 13:47:17 +0000

    Add create for other app apis

--- api/announcement.go
+++ api/announcement.go
@@ -21,6 +21,22 @@ func (s *Server) CreateAnnouncement(ctx context.Context, in *npool.CreateAnnounc
 	return resp, nil
 }
 
+func (s *Server) CreateAnnouncementForOtherApp(ctx context.Context, in *npool.CreateAnnouncementForOtherAppRequest) (*npool.CreateAnnouncementForOtherAppResponse, error) {
+	info := in.GetInfo()
+	info.AppID = in.GetTargetAppID()
+
+	resp, err := crud.Create(ctx, &npool.CreateAnnouncementRequest{
+		Info: info,
+	})
+	if err != nil {
+		logger.Sugar().Errorw("create announcement error: %v", err)
+		return &npool.CreateAnnouncementForOtherAppResponse{}, status.Error(codes.Internal, "internal server error")
+	}
+	return &npool.CreateAnnouncementForOtherAppResponse{
+		Info: resp.Info,
+	}, nil
+}
+
 func (s *Server) UpdateAnnouncement(ctx context.Context, in *npool.UpdateAnnouncementRequest) (*npool.UpdateAnnouncementResponse, error) {
 	resp, err := crud.Update(ctx, in)
 	if err != nil {
@@ -37,4 +53,17 @@ func (s *Server) GetAnnouncementsByApp(ctx context.Context, in *npool.GetAnnounc
 		return &npool.GetAnnouncementsByAppResponse{}, status.Error(codes.Internal, "internal server error")
 	}
 	return resp, nil
+}
+
+func (s *Server) GetAnnouncementsByOtherApp(ctx context.Context, in *npool.GetAnnouncementsByOtherAppRequest) (*npool.GetAnnouncementsByOtherAppResponse, error) {
+	resp, err := crud.GetAnnouncementsByApp(ctx, &npool.GetAnnouncementsByAppRequest{
+		AppID: in.GetTargetAppID(),
+	})
+	if err != nil {
+		logger.Sugar().Errorw("get announcements by app error: %v", err)
+		return &npool.GetAnnouncementsByOtherAppResponse{}, status.Error(codes.Internal, "internal server error")
+	}
+	return &npool.GetAnnouncementsByOtherAppResponse{
+		Infos: resp.Infos,
+	}, nil
 }
